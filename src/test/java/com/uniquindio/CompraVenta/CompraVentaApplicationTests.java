3a64c92879e8e423ba5484945a7306496d604bfd
juan998h <juane.gutierrezh@uqvirtual.edu.co>
2026-09-16 21:21:48 -0500

    SE CREA LA ESCRUCTURA PARA PROYECTO FINAL

---
+++ src/test/java/com/uniquindio/CompraVenta/CompraVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.CompraVenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
