0f6bcf5943d7072abb3570f49184a8108335056b
juandanielvera <juand.veram1@uqvirtual.edu.co>
2026-09-22 21:38:47 -0500

    se agrega la regla de dominio execption

---
+++ src/main/java/com/uniquindio/CompraVenta/domain/exception/ReglaDominioExeception.java
@@ -0,0 +1,7 @@
+package com.uniquindio.CompraVenta.domain.exception;
+
+public class ReglaDominioException extends RuntimeException {
+    public ReglaDominioException(String mensaje) {
+        super(mensaje);
+    }
+}
